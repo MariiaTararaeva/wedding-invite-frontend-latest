1ef451449f753900f68d8cd3a6fb44dca588c812
Mariia Tararaeva <mariatararaeva@icloud.com>
2025-04-12 16:31:51 +0200

    Delete bijAndrea directory

--- bijAndrea/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="bg-gray-800 text-gray-100 p-4 text-center text-sm">
-        <p>Andreas High Tea &copy; 2025 – Utrecht, Nederland.</p>
-        <p>Adres: Wonderlandlaan 7, 3584 AB Utrecht | Tel: 030-1234567 | Email: info@andreashightea.nl</p>
-      </footer>
-    );
-  }
-  
--- bijAndrea/src/sanity/client.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@sanity/client';
-
-export const sanityClient = createClient({
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID, // from your .env file
-  dataset: import.meta.env.VITE_SANITY_DATASET || 'production',
-  apiVersion: '2023-01-01', // change to today's date or your schema version
-  useCdn: true, // use CDN for faster reads
-  token: import.meta.env.VITE_SANITY_API_TOKEN, // only if you want to use a token
-});
